25805b535b12a6b8b8096366e60186a7ada02844
nizza1 <108374160+nizza1@users.noreply.github.com>
2023-04-17 14:44:13 +0200

    input add boot

--- src/pages/boots.jsx
+++ src/pages/boots.jsx
@@ -1,5 +1,92 @@
+import axios from 'axios';
+import {useState , useEffect} from 'react'
+
+import './bootsStyle.css'
+
 function Boots() {
-    return <h2>Boots</h2>;
+
+    const URL = 'http://localhost:3000/api/v1/boats';
+
+    const [formData, setFormData] = useState({
+        bootnr: 0,
+        baujahr: 0,
+        seriennummer: "",
+        material: "",
+        bootart: "",
+      });
+
+    const handleChange = (event) => {
+        setFormData({
+          ...formData,
+          [event.target.name]: event.target.value,
+        });
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+          axios.post(`${URL}`, formData)
+          .then((response) => {
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+    
+    return (
+        <div className="boots">
+            <form onSubmit={handleSubmit}>
+    
+        <input
+        placeholder='Boot Number'
+          type="number"
+          id="bootnr"
+          name="bootnr"
+          onChange={handleChange}
+        />
+        <br />
+        
+        <input
+        placeholder='Baujahr'
+          type="number"
+          id="baujahr"
+          name="baujahr"
+          onChange={handleChange}
+        />
+        <br />
+     
+        <input
+        placeholder='Seriennummer'
+          type="text"
+          id="seriennummer"
+          name="seriennummer"
+          onChange={handleChange}
+        />
+        <br />
+        
+        <input
+        placeholder='Bootart'
+          type="text"
+          id="bootart"
+          name="bootart"
+          onChange={handleChange}
+        />
+        <br />
+
+        <label for='material'>
+        material 
+        </label>
+        <select id="material" name="material" value={formData.material} onChange={handleChange}>
+        <option value="GFK">GFK</option>
+        <option value="Holz">Holz</option>
+        <option value="Metall">Metall</option>
+        <option value="Pappe">Pappe</option>
+        <option value="Seelen">Seelen</option>
+      </select>
+        <button type="submit" onClick={handleSubmit}>Add Boot</button>
+      </form>
+        </div>
+    );
   }
   
   export default Boots;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,44 @@
+import './homeStyle.css';
+import axios from 'axios';
+import {useState , useEffect} from 'react'
+
+
 function Home() {
-    return <h2>Home</h2>;
+
+    const URL = 'http://localhost:3000/api/v1/';
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      axios
+        .get(`${URL}boats`)
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, []);
+
+    return (
+        <div className="home">
+            <h1>Dashboard</h1>
+            <div className="infoContainer">
+                <div className="infoBox">
+                    <p>Aktuelle Reservierungen</p>
+                    <p>10</p>
+                </div>
+                <div className="infoBox">
+                    <p>Verfügbare Boote</p>
+                    <p>8</p>
+                </div>
+                <div className="infoBox">
+                    <p>Gesamtanzahl Boote</p>
+                    <p>{data.length}</p>
+                </div>
+            </div>
+        </div>
+    )
   }
   
   export default Home;
