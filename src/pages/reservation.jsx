c6773d9aee5a8fc1b65a44544d5550485e2d79af
nizza1 <108374160+nizza1@users.noreply.github.com>
2023-04-18 13:27:20 +0200

    avsvt

--- src/pages/reservation.jsx
+++ src/pages/reservation.jsx
@@ -5,19 +5,29 @@ const URL = "http://localhost:3000/api/v1/";
 function Reservation() {
 	const URL = "http://localhost:3000/api/v1/";
 
+    const [resButton, setResButton] = useState(false)
+
 	const [data, setData] = useState([]);
 
 	const [filtered, setFiltered] = useState({});
+
+    const [reservation, setReservation] = useState([]);
 
 	const handelreservationSingle = (boatId) => {
 		console.log(boatId);
 		const filteredItem = data.find((item) => boatId === item._id);
 
-		console.log(filteredItem);
+		setFiltered(filteredItem);
+        console.log(filteredItem);
 
-		if (Object.keys(filtered).length > 0) {
-			setFiltered(filteredItem);
-		}
+        axios.post(`${URL}reservation`, filtered)
+              .then((response) => {
+                console.log(response.data);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          
 	};
 	useEffect(() => {
 		axios
@@ -28,22 +38,32 @@ function Reservation() {
 			.catch((error) => {
 				console.log(error);
 			});
-	}, []);
 
-	/*useEffect(() => {
-        axios.get(`${URL}reservation`)
+       axios.get(`${URL}reservation`)
         .then((response) => {
-            setAllReservation(response.data);
+            setReservation(response.data);
         })
         .catch((error) => {
-          console.log(error);
-        });
-    }, []);*/
+            console.log(error);
+        })
+	}, []);
+
+	
 
 	return (
 		<div className="reservation">
-			{data &&
+
+            <div className="reserviert">
+                <h1>reserviert</h1>
+                {reservation && reservation.map((reservation,i) => <ul key={i}>
+                <li>boot nummer{reservation.bootnr}</li>
+                </ul>)}
+            </div>
+            <div className="reservieren">
+                <h1>reservieren</h1>
+                {data &&
 				data.map((boate) => {
+    
 					return (
 						<ul key={boate._id}>
 							<li>boot nummer{boate.bootnr}</li>
@@ -52,16 +72,21 @@ function Reservation() {
 							<li>material:{boate.material}</li>
 							<li>seriennummer:{boate.seriennummer}</li>
 							<li>status:</li>
+                            <button disabled={resButton}>
+                            {reservation.find((item) => item.bootnr === boate.bootnr) ? 'reserviert' : "reservieren"}
+                            </button>
 							<button
 								onClick={() => {
 									handelreservationSingle(boate._id);
 								}}
-							>
+							>{}
 								reservieren
 							</button>
 						</ul>
 					);
 				})}
+            </div>
+			
 		</div>
 	);
 }
