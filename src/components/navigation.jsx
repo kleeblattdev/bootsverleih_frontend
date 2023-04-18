e69d6607d53bfab148f57d8b53ad8955dd17abbc
nizza1 <108374160+nizza1@users.noreply.github.com>
2023-04-18 15:59:49 +0200

    icons

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
 import './navigationStyle.css'
+
+import HomeIcon from '../../public/home.png'
+
+import ReservationIcon from '../../public/booking.png'
+
+import BoateIcon from '../../public/yacht.png'
 function Navigation  ()  {
     return ( 
         <nav className="nav">
         <ul>
          
           <li>
-            <Link to="/reservation">Reservierung übersicht</Link>
+            <Link to="/reservation"><img src={ReservationIcon} /></Link>
           </li>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/"><img src={HomeIcon} /></Link>
           </li>
           <li>
-            <Link to="/boots">Boots übersicht</Link>
+            <Link to="/boots"><img src={BoateIcon} /></Link>
           </li>
         </ul>
       </nav>
