ad08b0325a3a9e4bc66f111c86c3582813a34e02
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-04-18 11:16:19 +0200

    boat id as parameter

--- src/pages/reservation.jsx
+++ src/pages/reservation.jsx
@@ -1,38 +1,36 @@
-import {useState , useEffect} from 'react';
-import axios from 'axios';
-const URL = 'http://localhost:3000/api/v1/';
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+const URL = "http://localhost:3000/api/v1/";
 
 function Reservation() {
+	const URL = "http://localhost:3000/api/v1/";
 
-    const URL = 'http://localhost:3000/api/v1/';
-   
-    const [data , setData] = useState([]);
+	const [data, setData] = useState([]);
 
-    const [selectedBoatId , setSelectedBoatId] = useState([]);
+	const [filtered, setFiltered] = useState({});
 
-    const [filtered , setFiltered] = useState({});
+	const handelreservationSingle = (boatId) => {
+		console.log(boatId);
+		const filteredItem = data.find((item) => boatId === item._id);
 
-    const handelreservationSingle = ()=> {
-        const filteredItem = data.find((item) => selectedBoatId === item._id);
+		console.log(filteredItem);
 
-        setFiltered(filteredItem);
-        if (Object.keys(filtered).length > 0){
-            console.log(true);
-        }else {null}
-  
-    }
-    useEffect(() => {
-          axios.get(`${URL}boats`)
-          .then((response) => {
-            setData(response.data);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }, []);
+		if (Object.keys(filtered).length > 0) {
+			setFiltered(filteredItem);
+		}
+	};
+	useEffect(() => {
+		axios
+			.get(`${URL}boats`)
+			.then((response) => {
+				setData(response.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
 
-      /*useEffect(() => {
+	/*useEffect(() => {
         axios.get(`${URL}reservation`)
         .then((response) => {
             setAllReservation(response.data);
@@ -42,28 +40,30 @@ function Reservation() {
         });
     }, []);*/
 
+	return (
+		<div className="reservation">
+			{data &&
+				data.map((boate) => {
+					return (
+						<ul key={boate._id}>
+							<li>boot nummer{boate.bootnr}</li>
+							<li>baujahr:{boate.baujahr}</li>
+							<li>bootart:{boate.bootart}</li>
+							<li>material:{boate.material}</li>
+							<li>seriennummer:{boate.seriennummer}</li>
+							<li>status:</li>
+							<button
+								onClick={() => {
+									handelreservationSingle(boate._id);
+								}}
+							>
+								reservieren
+							</button>
+						</ul>
+					);
+				})}
+		</div>
+	);
+}
 
-    return (
-        <div className='reservation'>
-            
-        {data && data.map((boate)=> {
-            return(
-                <ul key={boate._id
-                }>
-                    <li>boot nummer{boate.bootnr}</li>
-                    <li>baujahr:{boate.baujahr}</li>
-                    <li>bootart:{boate.bootart}</li>
-                    <li>material:{boate.material}</li>
-                    <li>seriennummer:{boate.seriennummer}</li>
-                    <li>status:</li>
-                    <button onClick={() => {setSelectedBoatId(boate._id);
-                    handelreservationSingle()}}>reservieren</button>
-                </ul>
-            )
-            
-        })}
-        </div>
-    );
-  }
-  
-  export default Reservation;
+export default Reservation;
